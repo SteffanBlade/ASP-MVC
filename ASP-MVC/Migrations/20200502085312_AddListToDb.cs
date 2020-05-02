4f6567300fcacf045b2c4b7ce62f7cdc75b35f20
Mihai <mihaiionstefan@gmail.com>
2020-05-02 11:59:55 +0300

    Added Db connection

---
+++ ASP-MVC/Migrations/20200502085312_AddListToDb.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ASP_MVC.Migrations
+{
+    public partial class AddListToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ListItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    itemName = table.Column<string>(nullable: false),
+                    time = table.Column<int>(nullable: false),
+                    value = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ListItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ListItems");
+        }
+    }
+}
---
+++ ASP-MVC/Models/ListItem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_MVC.Models
+{
+    public class ListItem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string itemName { get; set; }
+        public int time { get; set; }
+        public int value { get; set; }
+    
+    
+    }
+
+}
